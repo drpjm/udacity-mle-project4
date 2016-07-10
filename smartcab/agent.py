0ab180b5d7ca7e25704e8f329087f34153dc5c8d
Patrick Martin <patrickmartin@fastmail.com>
2016-07-10 14:39:44 -0400

    Implemented the Qtable updating and eps-greedy action selection policy. Appears to build Qtable correctly.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
@@ -9,10 +10,12 @@ class LearningAgent(Agent):
     def __init__(self, env):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
-        self.eps = 0.2 # some % of the time, choose a random action
+        self.eps = 0.5 # some % of the time, choose a random action
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         self.possible_actions = [None, 'forward', 'left', 'right']
         self.possbible_states = ['red', 'green']
+        self.gamma = 0.5 # Discount factor
+        self.curr_action = None
         self.qtable = {}
         # Init the qtable values to 0
         for s in self.possbible_states:
@@ -25,37 +28,59 @@ class LearningAgent(Agent):
         # TODO: Prepare for a new trip; reset any variables here, if required
 
     def update(self, t):
-        # Gather inputs
+        # SENSE
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
+        alpha = 1.0 / (t + 1)
+        print alpha
         # State update:
-        self.state = inputs['light'];
+        self.state = inputs['light']
+        print "*** Agent state = "
+        print self.state
 
         # First: random action selection:
-        rand_action_idx = random.randint(0,3)
-        action = self.possible_actions[rand_action_idx]
+        # rand_action_idx = random.randint(0,3)
+        # currAction = self.possible_actions[rand_action_idx]
+
+        # ACT
+        roll = random.random();
+        if roll < self.eps:
+            rand_action_idx = random.randint(0,3)
+            print "== Random move =="
+            print self.possible_actions[rand_action_idx]
+            self.curr_action = self.possible_actions[rand_action_idx]
+        else:
+            qVals = np.zeros(len(self.possible_actions))
+            for i in range(0, len(self.possible_actions)):
+                qVals[i] = self.qtable[(self.state, self.possible_actions[i])]
+            action_idx = np.argmax(qVals)
+            print "== Greedy move =="
+            print self.possible_actions[action_idx]
+            print np.max(qVals)
+            self.curr_action = self.possible_actions[action_idx]
 
         # Execute action and get reward
-        reward = self.env.act(self, action)
+        reward = self.env.act(self, self.curr_action)
+        print "*** Agent reward = "
+        print reward
+
+        # Update state *after* the action.
+        new_inputs = self.env.sense(self)
+        new_state = new_inputs['light']
         # Update the qtable accordingly
-        sa_pair = (self.state, action)
-        print "Qtable entry before: "
-        print self.qtable[sa_pair]
-        print "Qtable entry after: "
-        self.qtable[sa_pair] = reward
-        print self.qtable[sa_pair]
+        sa_pair = (self.state, self.curr_action)
+        # Q-table UPDATE
+        currQ = self.qtable[sa_pair]
+        # Compute max Q over all state, action pairs.
+        qVals = np.zeros(len(self.possible_actions))
+        for idx in range(0, len(self.possible_actions)):
+            qVals[idx] = self.qtable[(new_state, self.possible_actions[idx])]
+        maxNextQ = np.amax(qVals)
+        self.qtable[sa_pair] = (1-alpha)*currQ + alpha*(reward + self.gamma*maxNextQ)
 
-        # TODO: Learn policy based on state, action, reward
-        roll = random.random();
-        print roll
-        if roll < self.eps:
-            print "* Choose random action."
-        else:
-            print "* Q learning policy."
-
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
-
+        print self.qtable
+        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, self.curr_action, reward)  # [debug]
 
 def run():
     """Run the agent for a finite number of trials."""
@@ -63,7 +88,7 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # set agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
 
     # Now simulate it
     sim = Simulator(e, update_delay=1.0)  # reduce update_delay to speed up simulation
